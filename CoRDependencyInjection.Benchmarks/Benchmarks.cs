7c97ba56f9e8bdac0c6d9b7078e93dcb843e1b26
savedo <edoardo.savini@fmi.fc.it>
2026-03-09 15:55:22 +0100

    chore: improved benchmarks

--- CoRDependencyInjection.Benchmarks/Benchmarks.cs
+++ CoRDependencyInjection.Benchmarks/Benchmarks.cs
@@ -1,28 +1,56 @@
-﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes;
 using CoRDependencyInjection.Extensions;
 using CoRDependencyInjection.Tests.TestItems;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoRDependencyInjection.Benchmarks;
 
+[MemoryDiagnoser]
 public class Benchmarks
 {
-    private readonly IServiceProvider _serviceProvider;
-    
-    public Benchmarks()
+    private IServiceProvider _serviceProvider = null!;
+
+    [GlobalSetup]
+    public void Setup()
     {
         var serviceCollection = new ServiceCollection();
-        
+
         serviceCollection.AddTransientChain<ITestChain>()
-            .WithHandler<FirstHandler>()
-            .WithHandler<FirstHandler>()
             .WithHandler<FirstHandler>()
             .WithHandler<LastHandler>()
             .BuildChain();
-        
+
+        serviceCollection.AddScopedChain<IScopedTestChain>()
+            .WithHandler<ScopedFirstHandler>()
+            .WithHandler<ScopedLastHandler>()
+            .BuildChain();
+
+        serviceCollection.AddSingletonChain<ISingletonTestChain>()
+            .WithHandler<SingletonFirstHandler>()
+            .WithHandler<SingletonLastHandler>()
+            .BuildChain();
+
         _serviceProvider = serviceCollection.BuildServiceProvider();
     }
 
     [Benchmark]
-    public void TransitChain() => _serviceProvider.GetRequiredService<ITestChain>();
+    public void TransientChain() => _serviceProvider.GetRequiredService<ITestChain>();
+
+    [Benchmark]
+    public void ScopedChain()
+    {
+        using var scope = _serviceProvider.CreateScope();
+        scope.ServiceProvider.GetRequiredService<IScopedTestChain>();
+    }
+
+    [Benchmark]
+    public void SingletonChain() => _serviceProvider.GetRequiredService<ISingletonTestChain>();
 }
+
+public interface IScopedTestChain : ITestChain { }
+public class ScopedFirstHandler(IScopedTestChain next) : FirstHandler(next), IScopedTestChain;
+public class ScopedLastHandler() : LastHandler, IScopedTestChain;
+
+public interface ISingletonTestChain : ITestChain { }
+public class SingletonFirstHandler(ISingletonTestChain next) : FirstHandler(next), ISingletonTestChain;
+public class SingletonLastHandler() : LastHandler, ISingletonTestChain;
